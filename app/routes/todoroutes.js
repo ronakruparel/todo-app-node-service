f9712ecbb94447e24d35fc2e93058fa090d83143
ronakruparel <ronak.ruparel@accionlabs.com>
2019-08-14 21:16:26 +0530

    feat(todo):added GET all todos

--- app/routes/todoroutes.js
+++ app/routes/todoroutes.js
@@ -6,16 +6,21 @@ const conn = require("../../dbConfig");
 
 const app = express();
 
-app.get("/:id", (req, res) => {
+app.get("/", (req, res) => {
   if (!req.headers.authorization) {
     res.status(401).send({ status: 1, message: "Unauthorized" });
   } else {
     //decode token
     let buff = new Buffer(req.headers.authorization, "base64");
     let userData = JSON.parse(buff.toString("ascii"));
+
     let query = `SELECT  * from bucket LEFT JOIN  user_bucket_todo ON bucket.bucket_id = user_bucket_todo.bucket_id LEFT JOIN todo ON user_bucket_todo.todo_id = todo.todo_id WHERE user_id = "${
       userData.user_id
-    }" AND bucket.bucket_id = "${req.params.id}" `;
+    }" ${
+      req.query.bucket_id
+        ? `AND bucket.bucket_id = "${req.query.bucket_id}"`
+        : " "
+    } `;
     conn.query(query, (err, response) => {
       if (!err) {
         res.status(200).send({ status: 0, data: response });
