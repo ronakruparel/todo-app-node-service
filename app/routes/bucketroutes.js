8aac86173e0a4256acd1d4544d875c00b95b1fb3
ronakruparel <ronak.ruparel@accionlabs.com>
2019-08-14 20:27:32 +0530

    feat(bucket):added put and delete for buckets

--- app/routes/bucketroutes.js
+++ app/routes/bucketroutes.js
@@ -53,10 +53,14 @@ app.get("/", (req, res) => {
       res.status(500).send({ status: 1, message: "Internal Server error" });
     } else {
       if (!req.headers.authorization) {
+        //check authrized access
         res.status(401).send({ status: 1, message: "Unauthorized" });
       } else {
+        //decode token
         let buff = new Buffer(req.headers.authorization, "base64");
         let userData = JSON.parse(buff.toString("ascii"));
+        //join query to get data from association table
+
         let query = `SELECT * from bucket LEFT JOIN  user_bucket_todo ON bucket.bucket_id = user_bucket_todo.bucket_id WHERE user_id = "${
           userData.user_id
         }" `;
@@ -66,6 +70,64 @@ app.get("/", (req, res) => {
             res.status(200).send({ status: 0, data: response });
           } else {
             res.status(200).send({ status: 1, message: "Cannot get buckets" });
+          }
+        });
+      }
+    }
+  });
+});
+
+app.put("/", (req, res) => {
+  conn.getConnection((err, sql) => {
+    if (err) {
+      console.log(err.stack);
+      res.status(500).send({ status: 1, message: "Internal Server error" });
+    } else {
+      //check access
+      if (!req.headers.authorization) {
+        res.status(401).send({ status: 1, message: "Unauthorized" });
+      } else {
+        let buff = new Buffer(req.headers.authorization, "base64");
+        let userData = JSON.parse(buff.toString("ascii"));
+
+        let query = `UPDATE bucket SET bucket_name = "${
+          req.body.bucket_name
+        }" , current_status="${req.body.current_status}" WHERE bucket_id = "${
+          req.body.bucket_id
+        }"`;
+
+        sql.query(query, (err, response) => {
+          if (err) {
+            response
+              .status(200)
+              .send({ status: 1, message: "Cannot update bucket" });
+          } else {
+            res
+              .status(200)
+              .send({ status: 0, message: "Updated successfully" });
+          }
+        });
+      }
+    }
+  });
+});
+
+app.delete("/", (req, res) => {
+  conn.getConnection((err, sql) => {
+    if (err) {
+      res.status(500).send({ status: 1, message: "Internal Server error" });
+    } else {
+      if (!req.headers.authorization) {
+        res.status(401).send({ status: 1, message: "Unauthorized" });
+      } else {
+        let query = `DELETE FROM bucket WHERE bucket_id=${req.body.bucket_id}`;
+        sql.query(query, (err, response) => {
+          if (err) {
+            res.status(200).send({ status: 1, message: "Cannot delete" });
+          } else {
+            res
+              .status(200)
+              .send({ status: 0, message: "deleted Successfully" });
           }
         });
       }
