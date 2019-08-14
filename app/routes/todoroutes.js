5bc3d26c836327ac583c5929850153b0699a2448
ronakruparel <ronak.ruparel@accionlabs.com>
2019-08-14 21:39:06 +0530

    feat(todo):added POST request for todo

--- app/routes/todoroutes.js
+++ app/routes/todoroutes.js
@@ -25,7 +25,37 @@ app.get("/", (req, res) => {
       if (!err) {
         res.status(200).send({ status: 0, data: response });
       } else {
-        res.status(200).send({ status: 1, message: "cannot get" });
+        res.status(200).send({ status: 1, message: "cannot get todos" });
+      }
+    });
+  }
+});
+
+app.post("/", (req, res) => {
+  if (!req.headers.authorization) {
+    res.status(401).send({ status: 1, message: "Unauthorized" });
+  } else {
+    //decode token
+    let buff = new Buffer(req.headers.authorization, "base64");
+    let userData = JSON.parse(buff.toString("ascii"));
+    let query = `INSERT INTO todo SET title="${req.body.title}" , content="${
+      req.body.content
+    }"`;
+    conn.query(query, (err, response) => {
+      if (!err) {
+        let query = `INSERT INTO user_bucket_todo SET user_id="${
+          userData.user_id
+        }", bucket_id="${req.body.bucket_id}",todo_id="${response.insertId}"`;
+        conn.query(query, (err, resp) => {
+          if (!err) {
+            res.status(200).send({ status: 0, message: "Added" });
+          } else {
+            console.log(err.stack);
+          }
+        });
+      } else {
+        console.log(err);
+        res.status(200).send({ status: 1, message: "cannot add todos" });
       }
     });
   }
